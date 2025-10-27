0bf3448242ef500f340e642032591c8716f5d3b6
s1120097-ciga <s1120097@dmhs.kh.edu.tw>
2025-10-27 11:05:10 +0800

    Add files via upload

--- hw2.cpp
+++ hw2.cpp
@@ -6,30 +6,14 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main()
-{
-     cout << "範例 1: 輸出 1 到 10" << endl;
-    for (int i = 1; i <= 10; i++) {
-        cout << i << " ";
-        
-    }
-    cout << "\n\n";
-    
-     cout << "範例 2: 計算 1 到 10 的總和" << endl;
-    int sum = 0;
-    for (int i = 1; i <= 10; i++) {
-        sum += i;
-    }
-     cout << "總和 = " << sum << "\n\n";
-     
-     cout << "範例 3: 輸出偶數 (2 ~ 10)" << endl;
-    for (int i = 2; i <= 10; i += 2) {
-        cout << i << " ";
-    }
-     cout << "\n";
+int main() {
+     int sum = 0;
+     for (int i = 1; i <= 50; i++) {
+         if ((i % 7) == 0) cout << i << " ";
+     }
 
     return 0;
 }
-     
